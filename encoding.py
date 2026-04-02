f8b85df74d23a1b813138832dc3325c8ef38b482
Nhi <nhipnu9507@ut.edu.vn>
2026-04-03 02:38:32 +0700

    sua doi Brand_encoded va model_encoded

--- encoding.py
+++ encoding.py
@@ -5,11 +5,19 @@ def encode_data(file_path):
     df = pd.read_csv(file_path)
 
     cat_cols = ['Transmission', 'Owner', 'FuelType']
-    
-    # One-Hot Encoding
-    df = pd.get_dummies(df, columns=[c for c in cat_cols if c in df.columns], drop_first=True, dtype=int)
-    global_mean = df['AskPrice'].mean() 
-    
+    # Chuyển 3 cái trên thành nhãn 0 vs 1 
+    cols_to_encode = []
+    for col in cat_cols:
+        if col in df.columns:
+            cols_to_encode.append(col)
+    df = pd.get_dummies(df, columns=cols_to_encode, drop_first=True, dtype=int)
+
+    """"
+    Mô hình đang biết trước kết quả do lấy trung bình giá xe để đại diễn cho thương hiệu
+        global_mean = df['AskPrice'].mean() 
+    Sửa Brand_encodel và model_encoded, thử nghiệm theo hướng gom nhón 20 thương hiệu xuất hiện nhiều nhất và 'other'
+    """
+    """ Cũ
     for col in ['Brand', 'model']:
         if col in df.columns:
             mean_map = df.groupby(col)['AskPrice'].mean()
@@ -18,20 +26,36 @@ def encode_data(file_path):
             df[f'{col}_encoded'] = df[f'{col}_encoded'].round(3)
     
             df = df.drop(col, axis=1)
-            
-    # Chuẩn hóa phân phối Target (AskPrice) bằng Log Transform
+    """
+    # giữ Brand vì chỉ loanh quanh ít hãng 
+    if 'Brand' in df.columns:
+        df = pd.get_dummies(df, columns=['Brand'], drop_first=True, dtype=int)
+
+    if 'model' in df.columns:
+        model_counts = df['model'].value_counts()
+        top_models = model_counts.nlargest(20).index
+
+        new_model_column = []
+        for value in df['model']:
+            if value in top_models:
+                new_model_column.append(value)
+            else:
+                new_model_column.append('other')
+        df['model'] = new_model_column
+        df = pd.get_dummies(df, columns=['model'], drop_first=True, dtype=int)
+
+    # log đổi từ round(3) qua 8 coi tăng độ chính xác k        
     df['AskPrice'] = np.log1p(df['AskPrice'])
-    df['AskPrice'] = df['AskPrice'].round(3)
+    df['AskPrice'] = df['AskPrice'].round(8)
     return df
 
 if __name__ == "__main__":
     path = "Data_CarPrice.csv"
     try:
         df_final = encode_data(path)
-        print("Dữ liệu đã mã hóa hoàn toàn thành số.")
+        print("Data đã mã hóa hoàn toàn thành số.")
         print(df_final.head())
         
-        # Lưu ra CSV 
         df_final.to_csv("Data_Ready_For_ML.csv", index=False)
         print("Đã tạo file Data_Ready_For_ML.csv")
     except Exception as e:
